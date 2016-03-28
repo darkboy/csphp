b7a5f4ce2afb25827dec855299c9940b27a4bfb7
Darkboy <yxh3906261@gmail.com>
2016-03-28 22:45:08 +0800

    init response

--- csphp/core/CspResponse.php
+++ csphp/core/CspResponse.php
@@ -79,7 +79,162 @@ class CspResponse{
         503 => "HTTP/1.1 503 Service Unavailable",
         504 => "HTTP/1.1 504 Gateway Time-out"
     );
+
+    public $mimetypes = array(
+        'ez' => 'application/andrew-inset',
+        'hqx' => 'application/mac-binhex40',
+        'cpt' => 'application/mac-compactpro',
+        'doc' => 'application/msword',
+        'bin' => 'application/octet-stream',
+        'dms' => 'application/octet-stream',
+        'lha' => 'application/octet-stream',
+        'lzh' => 'application/octet-stream',
+        'exe' => 'application/octet-stream',
+        'class' => 'application/octet-stream',
+        'so' => 'application/octet-stream',
+        'dll' => 'application/octet-stream',
+        'oda' => 'application/oda',
+        'pdf' => 'application/pdf',
+        'ai' => 'application/postscript',
+        'eps' => 'application/postscript',
+        'ps' => 'application/postscript',
+        'smi' => 'application/smil',
+        'smil' => 'application/smil',
+        'mif' => 'application/vnd.mif',
+        'xls' => 'application/vnd.ms-excel',
+        'ppt' => 'application/vnd.ms-powerpoint',
+        'wbxml' => 'application/vnd.wap.wbxml',
+        'wmlc' => 'application/vnd.wap.wmlc',
+        'wmlsc' => 'application/vnd.wap.wmlscriptc',
+        'bcpio' => 'application/x-bcpio',
+        'vcd' => 'application/x-cdlink',
+        'pgn' => 'application/x-chess-pgn',
+        'cpio' => 'application/x-cpio',
+        'csh' => 'application/x-csh',
+        'dcr' => 'application/x-director',
+        'dir' => 'application/x-director',
+        'dxr' => 'application/x-director',
+        'dvi' => 'application/x-dvi',
+        'spl' => 'application/x-futuresplash',
+        'gtar' => 'application/x-gtar',
+        'hdf' => 'application/x-hdf',
+        'js' => 'application/x-javascript',
+        'json' => 'application/json',
+        'skp' => 'application/x-koan',
+        'skd' => 'application/x-koan',
+        'skt' => 'application/x-koan',
+        'skm' => 'application/x-koan',
+        'latex' => 'application/x-latex',
+        'nc' => 'application/x-netcdf',
+        'cdf' => 'application/x-netcdf',
+        'sh' => 'application/x-sh',
+        'shar' => 'application/x-shar',
+        'swf' => 'application/x-shockwave-flash',
+        'sit' => 'application/x-stuffit',
+        'sv4cpio' => 'application/x-sv4cpio',
+        'sv4crc' => 'application/x-sv4crc',
+        'tar' => 'application/x-tar',
+        'tcl' => 'application/x-tcl',
+        'tex' => 'application/x-tex',
+        'texinfo' => 'application/x-texinfo',
+        'texi' => 'application/x-texinfo',
+        't' => 'application/x-troff',
+        'tr' => 'application/x-troff',
+        'roff' => 'application/x-troff',
+        'man' => 'application/x-troff-man',
+        'me' => 'application/x-troff-me',
+        'ms' => 'application/x-troff-ms',
+        'ustar' => 'application/x-ustar',
+        'src' => 'application/x-wais-source',
+        'xhtml' => 'application/xhtml+xml',
+        'xht' => 'application/xhtml+xml',
+        'zip' => 'application/zip',
+        'au' => 'audio/basic',
+        'snd' => 'audio/basic',
+        'mid' => 'audio/midi',
+        'midi' => 'audio/midi',
+        'kar' => 'audio/midi',
+        'mpga' => 'audio/mpeg',
+        'mp2' => 'audio/mpeg',
+        'mp3' => 'audio/mpeg',
+        'aif' => 'audio/x-aiff',
+        'aiff' => 'audio/x-aiff',
+        'aifc' => 'audio/x-aiff',
+        'm3u' => 'audio/x-mpegurl',
+        'ram' => 'audio/x-pn-realaudio',
+        'rm' => 'audio/x-pn-realaudio',
+        'rpm' => 'audio/x-pn-realaudio-plugin',
+        'ra' => 'audio/x-realaudio',
+        'wav' => 'audio/x-wav',
+        'pdb' => 'chemical/x-pdb',
+        'xyz' => 'chemical/x-xyz',
+        'bmp' => 'image/bmp',
+        'gif' => 'image/gif',
+        'ief' => 'image/ief',
+        'jpeg' => 'image/jpeg',
+        'jpg' => 'image/jpeg',
+        'jpe' => 'image/jpeg',
+        'png' => 'image/png',
+        'tiff' => 'image/tiff',
+        'tif' => 'image/tiff',
+        'djvu' => 'image/vnd.djvu',
+        'djv' => 'image/vnd.djvu',
+        'wbmp' => 'image/vnd.wap.wbmp',
+        'ras' => 'image/x-cmu-raster',
+        'pnm' => 'image/x-portable-anymap',
+        'pbm' => 'image/x-portable-bitmap',
+        'pgm' => 'image/x-portable-graymap',
+        'ppm' => 'image/x-portable-pixmap',
+        'rgb' => 'image/x-rgb',
+        'xbm' => 'image/x-xbitmap',
+        'xpm' => 'image/x-xpixmap',
+        'xwd' => 'image/x-xwindowdump',
+        'igs' => 'model/iges',
+        'iges' => 'model/iges',
+        'msh' => 'model/mesh',
+        'mesh' => 'model/mesh',
+        'silo' => 'model/mesh',
+        'wrl' => 'model/vrml',
+        'vrml' => 'model/vrml',
+        'css' => 'text/css',
+        'html' => 'text/html',
+        'htm' => 'text/html',
+        'asc' => 'text/plain',
+        'txt' => 'text/plain',
+        'rtx' => 'text/richtext',
+        'rtf' => 'text/rtf',
+        'sgml' => 'text/sgml',
+        'sgm' => 'text/sgml',
+        'tsv' => 'text/tab-separated-values',
+        'wml' => 'text/vnd.wap.wml',
+        'wmls' => 'text/vnd.wap.wmlscript',
+        'etx' => 'text/x-setext',
+        'xsl' => 'text/xml',
+        'xml' => 'text/xml',
+        'mpeg' => 'video/mpeg',
+        'mpg' => 'video/mpeg',
+        'mpe' => 'video/mpeg',
+        'qt' => 'video/quicktime',
+        'mov' => 'video/quicktime',
+        'mxu' => 'video/vnd.mpegurl',
+        'avi' => 'video/x-msvideo',
+        'movie' => 'video/x-sgi-movie',
+        'ice' => 'x-conference/x-cooltalk',
+    );
     public function __construct(){
+    }
+
+    /**
+     * 更改响应数据
+     * @param $str 响应字符串
+     * @param bool $isAppend    是否追加到末尾
+     */
+    public function setBodyStr($str, $isAppend=false){
+        if($isAppend){
+            $this->bodyStr.= $str;
+        }else{
+            $this->bodyStr = $str;
+        }
     }
 
     /**
@@ -176,16 +331,33 @@ class CspResponse{
     }
 
     /**
-     * 更改响应数据
-     * @param $str 响应字符串
-     * @param bool $isAppend    是否追加到末尾
+     * 发送一个文件给客户端，浏览器端通常表现为 打开下载窗口
+     * @param $file             实际要发送的文件路径
+     * @param null $saveName    自动保存时的文件名
+     * @param string $contentType   内容类型,可以是 $this->mimetypes 的Key
      */
-    public function setBodyStr($str, $isAppend=false){
-        if($isAppend){
-            $this->bodyStr.= $str;
-        }else{
-            $this->bodyStr = $str;
+    public function sendFile($file, $saveName=null, $contentType='application/octet-stream'){
+        if(isset($this->mimetypes[$contentType])){
+            $contentType = $this->mimetypes[$contentType];
         }
+        $this->setHeader('Content-Type', $contentType);
+        if($saveName===null){
+            $saveName = basename($file);
+        }
+        $this->setHeader('Content-Disposition: attachment; filename="' . $saveName . '"');
+        header('Content-Transfer-Encoding: binary');
+        readfile($file);
+    }
+
+    /**
+     * 设置一个 content-type
+     * @param $typeOrName 可能是一个标准的 content-type 值或者 名称 如 json
+     */
+    public function setContentType($typeOrName){
+        if(isset($this->mimetypes[strtolower($typeOrName)])){
+            $typeOrName = $this->mimetypes[$typeOrName];
+        }
+        $this->setHeader('Content-Type', $typeOrName);
     }
 
     /**
