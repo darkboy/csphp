0d99c6a26d07a201ea9ed80450219be67141e205
Darkboy <yxh3906261@gmail.com>
2016-03-29 17:36:17 +0800

    test phpstorm

--- app/config/main.cfg.php
+++ app/config/main.cfg.php
@@ -3,21 +3,28 @@ namespace App;
 //配置示例
 $appConfig = array(
     //应用名称
-    'app_name'    =>'demo app',
+    'app_name'      =>'demo app',
     //应用的版本号 规则为 Ymd.no.svnno
-    'app_version' =>'20160301.01.xxxx',
+    'app_version'   =>'20160301.01.xxxx',
 
     //应用的根目录
     'app_base_path' =>dirname(__DIR__),
     //应用配置目录
     'app_cfg_path'  =>__DIR__,
     //应用命名空间
-    'app_namespace'=>__NAMESPACE__,
+    'app_namespace' =>__NAMESPACE__,
 
     //在框架引导期间 会自动加载的目录或者文件，文件或者目录为key 值为加载的目录深度,如果加载目录为文件则后面的值无效，始终为 0
     'auto_include_path'=>array(
         //fileOrDir=>level
     ),
+
+    //路径别名配置
+    'alias_path_config'=>array(
+        //@aliasname=>array(path-实际路径,nsPrefix-命名空间不需要时置空)
+        '@demo'=>array('@cfg/demo','')
+    ),
+
     //应用所用的host url 前缀，用于组装URL，拼接静态文件，等
     'host_key'=>array(
         '_default'	=>'http://www.domain.com/',
@@ -27,44 +34,16 @@ $appConfig = array(
         'api'		=>'http://api.domain.com/',
 
     ),
+    //主页
+    'home_url'      =>'/',
 
-    //路径别名配置
-    'alias_path_config'=>array(
-        //@aliasname=>path
-        '@demo'=>'@cfg/demo',
-    ),
 
+    //配置中引用配置
     'demo_for_file'=>array(
-
         '@myext'=>'-:mysql'
     ),
 
-    //应用组件配置列表
-    'components'=>array(
 
-        //这个 key 供后续在应用中可以通过 Csphp::comp($access_key) 引用组件
-        'access_key'=>array(
-            //类对象路由
-            'class' =>'@comp/demoComp',
-            //请求过滤器，在什么条件下使用该组件 !filter 则不在过滤中使用组件, 详见 过滤器描述
-            'filter'=>array(
-                //这条规则表示访问 /user/* 时启用这个组件
-                'match'=>"/user/*",
-                //这条规则表示，只在本机使用
-                'ip'=>"127.0.0.1,::1",
-                //这条规则表示只在 domain.com 下使用
-                'host'=>'*.domain.com'
-            ),
-            //组件的配置选项字典列表，每个key将作来组件的属性被赋值
-            'options'=>array(
-
-            )
-
-        ),
-
-
-
-    ),
 
 
 
@@ -113,3 +92,4 @@ $systemCfg = array(
 //以下配置将会 覆盖 系统配置
 $appConfig['system_config_over_write'] = $systemCfg;
 return $appConfig;
+
