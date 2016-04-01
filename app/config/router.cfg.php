1d06e5aa20bdf0a40b36426c68f4fd8d7bf62b10
Darkboy <yxh3906261@gmail.com>
2016-04-01 19:18:10 +0800

    update router feature

--- app/config/router.cfg.php
+++ app/config/router.cfg.php
@@ -35,11 +35,11 @@ return array(
             '/user/{uid-d-2,5}'     =>'/account/view',
             //后缀规则,如下规则与 /user/* 相同，只是增加了变量引用
             '/user/{var-*}'         =>'/other/{var}',
-            //fnmatch 规则，无变量可用
+            //通配符规则，无变量可用
             '/match/*'              =>'/match',
-            //选择语法1，如下表示，只有 /swyes/y1 和 /swyes/y2 会被匹配，/swyes/y3 等不匹配
+            //选择语法1，如下表示， 只有 /swyes/y1 和 /swyes/y2 会被匹配，例如 /swyes/y3 是不匹配的
             '/swyes/{vn-(y1|y2)}'   =>'/switch/catch_yes_sw1_sw2/{vn}',
-            //选择语法2,跟上面相反，是除了 /swno/n1 和 /swno/n2 外 ，/swno/n3 是匹配的
+            //选择语法2,跟上面相反，是除了 /swno/n1 和 /swno/n2 外 ，例如 /swno/n3 是匹配的
             '/swno/{vn-(?!n1|n2)}'  =>'/switch/catch_no_n1_n2/{vn}'
         )
     ),
