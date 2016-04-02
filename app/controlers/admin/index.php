0becee1c15ce1fd1ccbddf5d8f2e41c0569ad1d5
Darkboy <yxh3906261@gmail.com>
2016-04-02 13:26:31 +0800

    complete http do action func

--- app/controlers/admin/index.php
+++ app/controlers/admin/index.php
@@ -1,2 +1,31 @@
 <?php
+namespace App\controlers\admin;
+use \Csp\base\CspBaseControler;
+use \Csphp;
+
+class index extends CspBaseControler{
+    public $var= 'test';
+
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function filter(){
+        echo "\nfilter run...\n";
+    }
+
+    public function beforeAction(){
+        echo "\nbeforeAction\n";
+    }
+    public function afterAction(){
+        echo "\nafterAction\n";
+    }
+
+    public function actionGet(){}
+
+    public function actionIndex(){
+        echo 'Hello word index';
+    }
+
+}
 
