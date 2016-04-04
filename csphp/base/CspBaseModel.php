2ad5ed34885cf405fed65331957e10bca2f4b752
Darkboy <yxh3906261@gmail.com>
2016-04-04 15:48:28 +0800

    init model struct

--- csphp/base/CspBaseModel.php
+++ csphp/base/CspBaseModel.php
@@ -4,9 +4,61 @@ use \Csphp;
 
 class CspBaseModel {
 
-    public $jsonpCallbackName = null;
+    protected $__curDB = null;
+    /**
+     * 模型字段描述
+     * @var array
+     */
+    protected $__tableFields = array();
+    /**
+     * 数据格式描述
+     * @var array
+     */
+    protected $__formater = array();
 
-    public function __construct() {
+    public function __construct($tableName,$dbName,$pkName = 'id' ) {
+        $this->db = Csphp::comp('mysql');
+    }
+
+
+    protected function initFields($fields=array()){
 
     }
+    protected function initFormaters($formater=array()){
+
+    }
+
+    public function save($data=null){}
+    public function insert($data=null){}
+    public function create($data=null){}
+    public function batchInsert($data=null){}
+    public function batchCreate($data=null){}
+    public function batchSave($data=null){}
+
+    public function update($data=null){}
+    public function delete($condOrPk){}
+    public function pks($cond){}
+
+    public function get($condOrPk){}
+    public function gets($condOrPks){}
+    public function formatGet($fk,$condOrPk){}
+    public function formatGets($fk,$condOrPks){}
+
+
+    public function find($cond,$page,$pageSize){}
+    public function formatFind($cond,$page,$pageSize){}
+
+    public function dataFormater($data, $isMulti=false){}
+
+
+    public function __get() {
+
+    }
+
+
+    public function __set() {
+
+    }
+
+    public function __call(){}
 }
