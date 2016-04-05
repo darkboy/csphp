4f5dbaf38a970a73b25ed490ea1c7ec06c3bb398
Darkboy <yxh3906261@gmail.com>
2016-04-05 11:56:30 +0800

    update mysqli db feature

--- csphp/base/CspBaseModel.php
+++ csphp/base/CspBaseModel.php
@@ -7,6 +7,27 @@ use \Csp\comp\CspCompDBMysqli;
 class CspBaseModel {
 
     /**
+     * 减少模型属性，将基类要用到的属性封装在一个总配置中，访问，模型使用对象模式
+     * @var array
+     */
+    protected $___modBaseAttr = array(
+        //当前模型使用的 db 配置名 通过是 数据名
+        'db'=>null,
+        //当前模型对应的表名
+        'tb'=>null,
+        //主键 字段名
+        'pk'=>null,
+        //是否使用缓存 false true string
+        'use_cache'=>false,
+        //当前模型: 'fieldName'=>array('typeOrFormat', 'create_validator','update_validator','default')
+        'tb_fields'=>array(),
+        //模型数据格式定义
+        'formaters'=>array(),
+        //模型关系定义
+        'relations'=>array()
+    );
+
+    /**
      * @var CspCompDBMysqli
      */
     protected $__DB = null;
@@ -17,7 +38,7 @@ class CspBaseModel {
      * @var array
      */
     protected $__tableFields = array(
-        //'fieldName'=>array('format', 'create_validator','update_validator'),
+        //'fieldName'=>array('typeOrFormat', 'create_validator','update_validator'),
     );
     /**
      * 数据格式描述配置
@@ -28,19 +49,21 @@ class CspBaseModel {
 
 
     /**
-     * @param $tbName
-     * @param string $pkName
-     * @param bool $useCache
-     * @param string $dbCfgName
+     * @param string    $tbName
+     * @param string    $pkName
+     * @param bool      $useCache
+     * @param string    $dbCfgName
      * @throws \Csp\core\CspException
      */
     public function initModel($tbName, $pkName='id', $useCache=false, $dbCfgName = null ) {
-        $this->__pk = $pkName;
-        $this->__TB = $tbName;
-        $this->__DB = CspCompDBMysqli::getInstance($dbCfgName);
+        $this->setOption('pk', $pkName);
+        $this->setOption('tb', $tbName);
+        $this->setOption('db', CspCompDBMysqli::getInstance($dbCfgName));
+        $this->setOption('use_cache', $useCache);
 
         return $this;
     }
+
 
     /**
      * 如果未对数据表定义对应的 model 也可以直接 获取对应的模型操作
@@ -55,6 +78,20 @@ class CspBaseModel {
         return $mObj->initModel($tbName, $pkName, $useCache, $dbCfgName);
     }
 
+
+    public function setOption($k, $v){
+        $this->___modBaseAttr[$k] = $v;
+    }
+    public function getOption($k){
+        return isset($this->___modBaseAttr[$k]) ? $this->___modBaseAttr[$k] : null;
+    }
+
+    /**
+     * @return CspCompDBMysqli
+     */
+    public function db(){
+        return $this->getOption('db');
+    }
     /**
      * 初始化模型字段
      * @param array $fields
@@ -114,6 +151,8 @@ class CspBaseModel {
 
 
 
+
+    public function toJson($data=null){}
 
     public function save($data=null){}
     public function insert($data=null){}
