afb8468d271307c609bde050831ce8e8c18bfe5b
Darkboy <yxh3906261@gmail.com>
2016-05-17 11:26:21 +0800

    add CsphpJS  feature

---
+++ public/statics/core/CspJS.js
@@ -0,0 +1,16 @@
+//与 服务端通讯 用的 JS客户端库
+(function($,name){
+    var me = {};
+
+    me.prototype.pipe = function(){
+
+    };
+
+
+
+
+
+    me.prototype.ajax()
+    //
+    window[name] = me;
+})(jQuery,'CspJS');
