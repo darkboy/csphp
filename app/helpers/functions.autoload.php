af613ae7dd3cbd468db7a33a7193c5f4c4211035
Darkboy <yxh3906261@gmail.com>
2016-05-18 17:17:29 +0800

    add preload feature

--- app/helpers/functions.autoload.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-function preload_golbal_function_demo(){
-        echo 'Hello: '.__FUNCTION__;
-}
--- app/helpers/preload/demo.preload.php
+++ app/helpers/preload/demo.preload.php
@@ -1,5 +1,9 @@
 <?php
+/**
+ * 放在 app/helpers/ 目录中，并且 以 .preload.php 命名的php文件 将会被预加载
+ *
+ */
 
-function preload_golbal_function_demo(){
+function preload_golbal_function_demo2(){
     echo 'Hello: '.__FUNCTION__;
 }
