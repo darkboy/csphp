3663d0f9a7689c02add792e05d6ab7789f54c139
Darkboy <yxh3906261@gmail.com>
2016-05-21 16:32:06 +0800

    optimize env

--- app/config/router.cfg.php
+++ app/config/router.cfg.php
@@ -33,33 +33,33 @@ return array(
         //路由列表, 配置规则为： 路由模板=>目标路由
         'rule_list'=>array(
             //别名
-            '/user/alias'           => '/index',
+            '/rdemo/alias'           => '/index',
 
-            //闭包路由
-            '/user/func'            => function($request){echo 'hello world';},
+            //闭包路由: function (CspRequest $request){}
+            '/rdemo/func'            => function($request){echo 'hello world';},
 
-            //callable 绝对路由
-            '/user/abs'             => '@m-ctrl/demo::actionForAbsRouteDemo',
-            '/user/abs2'            => '\App\comtrolers\home\demo::actionForAbsRouteDemo',
-            '/user/abs3'            => 'demo::actionForAbsRouteDemo',
+            //对象路由, 指定路由对象
+            '/rdemo/abs'             => '@m-ctrl/demo::actionForRouteDemo',
+            '/rdemo/abs2'            => '\App\comtrolers\home\demo::actionForRouteDemo',
+            '/rdemo/abs3'            => 'demo::actionForRouteDemo',
 
             //变量与变量引用,如下规则将可实现迁移目的
-            '/user/{arg1}/{arg2}'   => '/account/{arg2}/{arg1}',
+            '/rdemo/{arg1}/{arg2}'   => '/account/{arg2}/{arg1}',
             //变量类型 和 长度 的限制
-            '/user/{uid-d-2,5}'     => '/account/view',
+            '/rdemo/{uid-d-2,5}'     => '/account/view',
 
             //后缀规则,如下规则与 /user/* 相同，只是增加了变量引用
-            '/user/{var-*}'         => '/other/{var}',
+            '/rdemo/{var-*}'         => '/other/{var}',
             //通配符规则，无变量可用
-            '/match/*'              => '/match',
+            '/rdemo/match/*'              => '/match',
 
             //选择语法1，如下表示， 只有 /swyes/y1 和 /swyes/y2 会被匹配，例如 /swyes/y3 是不匹配的
-            '/swyes/{vn-(y1|y2)}'   => '/switch/catch_yes_y1_y2/{vn}',
+            '/rdemo/swyes/{vn-(y1|y2)}'   => '/switch/catch_yes_y1_y2/{vn}',
             //选择语法2,跟上面相反，是除了 /swno/n1 和 /swno/n2 外 ，例如 /swno/n3 是匹配的
-            '/swno/{vn-(?!n1|n2)}'  => '/switch/catch_no_n1_n2/{vn}',
+            '/rdemo/swno/{vn-(?!n1|n2)}'  => '/switch/catch_no_n1_n2/{vn}',
 
             //正则表达式规则，当使用正则表达式时，必须以 # 号开头，即以 # 作为正则分隔符，里面的 命名捕获将作为路由变量 如下面的ID
-            '#^/regexp/(?<id>[\d]+)$#sim'    =>'/regexp/{id}'
+            '#^/rdemo/regexp/(?<id>[\d]+)$#sim'    =>'/demo/forRouteDemo'
         )
     ),
 
