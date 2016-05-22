10d3c4042325d8d5103984e593db8a946e8ac352
Darkboy <yxh3906261@gmail.com>
2016-05-22 08:58:40 +0800

    optimize comp

--- app/controlers/home/demo.php
+++ app/controlers/home/demo.php
@@ -91,5 +91,9 @@ class demo extends CspBaseControler{
         echo '<pre>路由解释示例,forRouteDemo 当前命中的路由信息如下:',"\n";print_r(Csphp::router()->getRouteInfo());
     }
 
+    public function actionComp(){
+        Csphp::comp('comp_demo')->hello();
+    }
+
 }
 
--- app/libs/libDemo.php
+++ app/libs/libDemo.php
@@ -3,9 +3,11 @@ namespace App\libs;
 use Csphp;
 use Csp\core\CspException;
 
-class libDemo{
+class libDemo {
+
+    public $opts = 'init';
     public function __construct() {
-        echo __CLASS__.' init';
+        //echo __CLASS__.' init';
     }
 
     public function handler(){
@@ -13,7 +15,11 @@ class libDemo{
     }
 
     public function setInitOptions($opts){
-        echo __CLASS__.' options '.json_encode($opts);
+        $this->opts = $opts;
+    }
+
+    public function hello(){
+        echo 'Hellp in libDemo comp with: '.json_encode($this->opts);
     }
 
 }
