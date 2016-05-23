a7f2dc4854b36f37cbf594232c12cdb3eef2914c
Darkboy <yxh3906261@gmail.com>
2016-05-23 20:44:27 +0800

    optimize middleware

--- csphp/core/CspPipeline.php
+++ csphp/core/CspPipeline.php
@@ -8,7 +8,7 @@ use Csp\core\CspException;
 class CspPipeline{
 
     /**
-     * The object being passed through the pipeline.
+     * 在 pipeline 中传递的对象
      *
      * @var mixed
      */
@@ -29,11 +29,11 @@ class CspPipeline{
     protected $method = 'handle';
 
     /**
-     * Create a new class instance.
      *
      * @return void
      */
     public function __construct() {
+
     }
 
     /**
@@ -105,17 +105,14 @@ class CspPipeline{
     protected function getSlice() {
         return function ($stack, $pipe) {
             return function ($passable) use ($stack, $pipe) {
-                // If the pipe is an instance of a Closure, we will just call it directly but
-                // otherwise we'll resolve the pipes out of the container and call it with
-                // the appropriate method and arguments, returning the results back out.
                 if ($pipe instanceof Closure) {
                     return call_user_func($pipe, $passable, $stack);
                 } else {
-                    /*
+
                     list($name, $parameters) = $this->parsePipeString($pipe);
-                    return call_user_func_array([$this->container->make($name), $this->method],
+                    return call_user_func_array([new $name, $this->method],
                         array_merge([$passable, $stack], $parameters));
-                    */
+
 
                 }
             };
