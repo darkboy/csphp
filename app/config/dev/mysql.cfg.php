2773a887356ad362f0663361d394a9d43e936505
Darkboy <yxh3906261@gmail.com>
2016-05-24 13:34:56 +0800

    optimize exception

--- app/config/dev/mysql.cfg.php
+++ app/config/dev/mysql.cfg.php
@@ -5,21 +5,22 @@
  * slave 的配置将会 与 主配置进行合并后作为从机的配置
  *
  * 配置示例 dsnName=>$dbConfig :
+ *
  * 'default'=>array(
  *      'is_default'=>true,
  *      'master'=>array(
- *      'charset'   =>'utf8',
- *      'user'      =>'root',
- *      'pwd'       =>'123456',
- *      'port'      =>'3306',
- *      'db_name'   =>'test',
- *      'tb_prefix' =>'csp_'
- *  ),
- *  'slaves'=>array(
- *      array('host'=>'slave1.db.host'),
- *      array('host'=>'slave2.db.host'),
+ *          'charset'   =>'utf8',
+ *          'user'      =>'root',
+ *          'pwd'       =>'123456',
+ *          'port'      =>'3306',
+ *          'db_name'   =>'test',
+ *          'tb_prefix' =>'csp_'
+ *      ),
+ *      'slaves'=>array(
+ *          array('host'=>'slave1.db.host'),
+ *          array('host'=>'slave2.db.host'),
+ *      )
  *  )
- * )
  */
 return array(
 
