b96af99aa5d21e27efcd7a6361655ce3a0a55c77
Darkboy <yxh3906261@gmail.com>
2016-05-24 11:34:38 +0800

    optimize exception

--- csphp/comp/db/CspCompDBConnection.php
+++ csphp/comp/db/CspCompDBConnection.php
@@ -3,12 +3,17 @@ namespace Csp\comp\db;
 
 use Csp\base\CspBaseComponent;
 use Csp\comp\db\CspCompDBMysqli;
+use Csp\core\CspException;
 use Csphp;
 
 
 
 class CspCompDBConnection extends CspBaseComponent {
 
+    /**
+     * mysqli 实例对象池
+     * @var array
+     */
     public static  $mysqlConnectionPoll = [];
 
     public function __construct() {
@@ -21,15 +26,52 @@ class CspCompDBConnection extends CspBaseComponent {
      * @param string $dbConfig
      * @return CspCompDBMysqli
      */
-    public static function getConnection($dsnName='default'){
-        if(!isset(self::$mysqlConnectionPoll[$dsnName])){
-            $dbConfig = Csphp::appCfg('mysql/'.$dsnName, []);
-            self::$mysqlConnectionPoll[$dsnName] = new CspCompDBMysqli($dbConfig);
+    public static function getConnection($dsnName=null){
+
+        if(isset(self::$mysqlConnectionPoll[$dsnName])){
+            return self::$mysqlConnectionPoll[$dsnName];
         }
+
+        if($dsnName===null){
+            $dsn = self::getDefaultDsnConfig();
+            $dsnName = $dsn['name'];
+            $dbConfig = $dsn['cfg'];
+        }else{
+            $dbConfig = Csphp::appCfg('mysql/'.$dsnName, []);
+        }
+        if(empty($dbConfig)){
+            throw new CspException("Error db config, dsnName: mysql/{$dsnName} ");
+        }
+        self::$mysqlConnectionPoll[$dsnName] = new CspCompDBMysqli($dbConfig);
         return self::$mysqlConnectionPoll[$dsnName];
     }
 
-    public static function tmp(){
-
+    /**
+     * 获取默认的数据库 DSN 配置 配置为 is_default 的第一个，
+     *
+     * @return array ['name'=>$n,'cfg'=>$v];
+     */
+    public static function getDefaultDsnConfig(){
+        static $defaultDsnName=null;
+        if($defaultDsnName!==null){
+            return Csphp::appCfg('mysql/'.$defaultDsnName, []);
+        }
+        $mysqlConfig = Csphp::appCfg('mysql', []);
+        $dbConfig = null;
+        $firstName = null;
+        foreach($mysqlConfig as $n=>$v){
+            if(isset($v['is_default']) && $v['is_default']){
+                $defaultDsnName = $n;
+                print_r($v);
+                return ['name'=>$n,'cfg'=>$v];
+            }
+            if(!$firstName){
+                $firstName = $n;
+            }
+        }
+        $defaultDsnName = $firstName;
+        return ['name'=>$firstName, 'cfg'=>$mysqlConfig[$firstName]];
     }
+
+
 }
