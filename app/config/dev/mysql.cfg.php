cc7ded3776d9a0bcc6eece8fccdb940bf4ff3004
Darkboy <yxh3906261@gmail.com>
2016-05-24 21:46:46 +0800

    optimize struct

--- app/config/dev/mysql.cfg.php
+++ app/config/dev/mysql.cfg.php
@@ -24,36 +24,37 @@
  */
 return array(
 
-    'default'=>array(
-        'is_default'=>true,
-        'master'=>array(
-            'charset'   =>'utf8',
-            'user'      =>'root',
-            'pwd'       =>'123456',
-            'port'      =>'3306',
-            'db_name'   =>'test',
-            'tb_prefix' =>'csp_'
+    'default' => array(
+        'is_default' => true,
+        'master'     => array(
+            'charset'   => 'utf8',
+            'user'      => 'root',
+            'pwd'       => '123456',
+            'port'      => '3306',
+            'db_name'   => 'test',
+            'tb_prefix' => 'csp_'
         ),
-        'slaves'=>array(
-            array('host'=>'slave1.db.host'),
-            array('host'=>'slave2.db.host'),
+        'slaves'     => array(
+            array('host' => 'slave1.db.host'),
+            array('host' => 'slave2.db.host'),
         )
     ),
 
-    'dsn_2'=>array(
-        'is_default'=>true,
-        'master'=>array(
-            'charset'   =>'utf8',
-            'user'      =>'root',
-            'pwd'       =>'123456',
-            'port'      =>'3306',
-            'db_name'   =>'test',
-            'tb_prefix' =>'csp_'
+    'dsn_2' => array(
+        'is_default' => true,
+        'master'     => array(
+            'charset'   => 'utf8',
+            'user'      => 'root',
+            'pwd'       => '123456',
+            'port'      => '3306',
+            'db_name'   => 'test',
+            'tb_prefix' => 'csp_'
         ),
-        'slaves'=>array(
-            array('host'=>'slave1.db.host'),
-            array('host'=>'slave2.db.host'),
+        'slaves'     => array(
+            array('host' => 'slave1.db.host'),
+            array('host' => 'slave2.db.host'),
         )
     ),
+
 );
 
---
+++ csphp/comp/store/CspCompFtp.php
@@ -0,0 +1,25 @@
+<?php
+namespace Csp\comp\store;
+use \Csp\base\CspBaseControler;
+use \Csp\base\CspBaseComponent;
+use \Csphp;
+
+
+/**
+ *
+ * Class CspCompRedis
+ * @package Csp\comp
+ */
+class CspCompRedis extends CspBaseComponent {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function start(){
+        //Csphp::request();
+    }
+
+
+
+}
